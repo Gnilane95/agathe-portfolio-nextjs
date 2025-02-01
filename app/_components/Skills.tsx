75e2bf79e6a5a24938aa8054c63037f088a3c196
Agathe Diouf <agathegnilanediouf@gmail.com>
2025-02-01 14:21:23 +0100

    Section skills modified

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -5,6 +5,7 @@ import { Badge } from "@/components/ui/badge";
 import { NextjsLogo } from "./icons/NextjsLogo";
 import { WordpressLogo } from "./icons/WordpressLogo";
 import { WixLogo } from "./icons/WixLogo";
+import { CodeXml, Paintbrush, Wrench } from "lucide-react";
 
 export const Skills = () => {
     return (
@@ -15,19 +16,22 @@ export const Skills = () => {
         </h2>
         <div className="flex max-md:flex-col gap-4">
             <div className="flex flex-col gap-2 flex-1">
-                <NextjsLogo size={42} className="animate-spin" style={{ animationDuration : "10s" }}/>
-                <h3 className="text-2xl font-semibold tracking-tight">NextJs</h3>
-                <p className="text-sm text-muted-foreground">Mon framework principal est <Code>NextJs</Code>. Je l&apos;utilise pour toute sorte d&apos;application web, en frontend comme en backend.</p>
+                {/* <NextjsLogo size={42} className="animate-spin" style={{ animationDuration : "10s" }}/> */}
+                <CodeXml size={52} />
+                <h3 className="text-2xl font-semibold tracking-tight">Création</h3>
+                <p className="text-sm text-muted-foreground">
+                    Vous avez un projet de site web en tête ? Je vous accompagne de A à Z pour concevoir un site moderne, performant et adapté à vos besoins. Que ce soit un <span className="font-semibold">site vitrine</span>, un <span className="font-semibold">portfolio</span> ou une <span className="font-semibold">boutique en ligne</span>, je mets mon expertise en développement web au service de votre réussite en ligne.
+                </p>
             </div>
             <div className="flex flex-col gap-2 flex-1">
-                <WordpressLogo size={42}/>
-                <h3 className="text-2xl font-semibold tracking-tight">Wordpress</h3>
-                <p className="text-sm text-muted-foreground">Pour créer ou faire la refonte de votre site <Code>Wordpress</Code>, vous êtes au bon endroit. Avec Kadence Pro, je vous conçois une site qualitatif et performant.</p>
+                <Paintbrush size={52} />
+                <h3 className="text-2xl font-semibold tracking-tight">Refonte</h3>
+                <p className="text-sm text-muted-foreground">Votre site actuel ne reflète plus votre image ou manque de performance ? Je vous aide à lui donner une nouvelle vie ! <span className="font-semibold">Amélioration du design</span>, <span className="font-semibold">optimisation des performances</span>, <span className="font-semibold">refonte UX/UI</span>… Je repense votre site pour qu’il soit plus attractif, rapide et efficace.</p>
             </div>
             <div className="flex flex-col gap-2 flex-1">
-                <WixLogo size={42} />
-                <h3 className="text-2xl font-semibold tracking-tight">wix</h3>
-                <p className="text-sm text-muted-foreground">Vous avez décidez de faire votre site vous même et voulez utiliser <Code>Wix</Code> ? Cependant, seule une bonne maitrise de la plateforme saura démarquer votre site. <Link href="#me-contacter"><Code>Contactez-moi</Code></Link> !</p>
+                <Wrench size={52} />
+                <h3 className="text-2xl font-semibold tracking-tight">Maintenance</h3>
+                <p className="text-sm text-muted-foreground">Un site web performant nécessite un suivi régulier. <span className="font-semibold">Mises à jour</span>, <span className="font-semibold">sécurité</span>, <span className="font-semibold">corrections de bugs</span>, <span className="font-semibold">amélioration du référencement</span>… Je veille à ce que votre site reste sécurisé, fonctionnel et toujours optimisé pour offrir la meilleure expérience à vos visiteurs.</p>
             </div>
         </div>
     </Section>
